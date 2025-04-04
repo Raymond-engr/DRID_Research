49f1b0490c9cf5aaa6843e5c7c84c444610c573b
Raymond-engr <raymondomoyakhi@gmail.com>
2025-04-04 00:51:36 -0500

    added the client folder

--- src/middleware/notFound.js
+++
@@ -1,8 +0,0 @@
-const notFound = (req, res, next) => {
-    const error = new Error(`Resource Not Found - ${req.originalUrl}`);
-    error.statusCode = 404;
-    error.isOperational = true;
-    next(error);
-  };
-  
-  export default notFound;
--- src/utils/asyncHandler.js
+++
@@ -1,7 +0,0 @@
-const asyncHandler = (execution) => {
-    return (req, res, next) => {
-      execution(req, res, next).catch(next);
-    };
-  };
-  
-  export default asyncHandler;
